55b3e2752b2bad1aeea76303d3021f36dddbc0ee
John Bender <john.m.bender@gmail.com>
2014-11-11 12:14:26 -0800

    3 down 3 to go

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,10 @@
 build(){
-  filename=`echo $1 | sed 's/\..*//'`
+  filename=`basename $1 | sed 's/\..*//'`
 	pdflatex -interaction=nonstopmode $1
   if ! [ -z "$2" ]; then
 	  bibtex $filename.aux
-	  pdflatex -interaction=nonstopmode $1
-	  pdflatex -interaction=nonstopmode $1
+	  pdflatex -interaction=nonstopmode $1 > /dev/null
+	  pdflatex -interaction=nonstopmode $1 > /dev/null
   fi
 }
 
